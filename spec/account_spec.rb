662c27d6edec7ea10850c89a8708dfb530db2094
Valeria Serci <balerty84@gmail.com>
2019-10-22 12:01:55 +0100

    Deposit increases balance

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,4 +4,9 @@ describe Account do
   it 'has balance 0' do
     expect(subject.balance).to eq(0)
   end
+  it 'increases balance when #deposit' do
+    subject.deposit(1000)
+    expect(subject.balance).to eq(1000)
+  end
+
 end
