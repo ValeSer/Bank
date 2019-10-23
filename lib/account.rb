14b5e031671a677d282107d757f36e0d69cac895
Valeria Serci <balerty84@gmail.com>
2019-10-23 10:41:26 +0100

    Update deposit and withdrawal to push info into bank_statement

--- lib/account.rb
+++ lib/account.rb
@@ -1,16 +1,26 @@
 class Account
-  attr_reader :balance
+  attr_reader :balance, :bank_statement
 
   def initialize
     @balance = 0
+    @bank_statement = []
   end
 
-  def deposit(amount)
+  def deposit(amount, date)
+    date
     @balance += amount
+    @bank_statement.push(date: date, credit: amount, debit: 0, balance: @balance)
   end
 
-  def withdrawal(amount)
+  def withdrawal(amount, date)
     @balance -= amount
+    @bank_statement.push(date: date, credit: 0, debit: amount, balance: @balance)
+  end
+
+private
+  def date
+    t = Time.now
+    t.strftime("%m/%d/%Y at %I:%M%p")
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,18 +5,33 @@ describe Account do
     expect(subject.balance).to eq(0)
   end
   it 'increases balance when #deposit' do
-    subject.deposit(1000)
+    subject.deposit(1000, '22/03/2018')
     expect(subject.balance).to eq(1000)
   end
   it 'makes #deposit when balance is not 0' do
-    subject.deposit(500)
-    subject.deposit(1000)
+    subject.deposit(500, '21/03/2018')
+    subject.deposit(1000, '22/03/2018')
     expect(subject.balance).to eq(1500)
   end
   it 'decreases balance when #withdrawal' do
-    subject.deposit(1000)
-    subject.withdrawal(500)
+    subject.deposit(1000, '21/03/2018')
+    subject.withdrawal(500, '22/03/2018')
     expect(subject.balance).to eq(500)
   end
+  it 'adds statement elements with deposit into @bank_statement' do
+    subject.deposit(1000, '21/03/2018')
+    statement = {date: '21/03/2018', credit: 1000, debit: 0, balance: 1000}
+    expect(subject.bank_statement[0]).to eq(statement)
+  end
+  it 'adds statement elements with withdrawal into @bank_statement' do
+    subject.deposit(1000, '21/03/2018')
+    subject.withdrawal(500, '22/03/2018')
+    statement = {date: '21/03/2018', credit: 1000, debit: 0, balance: 1000},
+      {date: '22/03/2018', credit: 0, debit: 500, balance: 500}
+    expect(subject.bank_statement).to eq(statement)
+  end
+
+
+
 
 end
